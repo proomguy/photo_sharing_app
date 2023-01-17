7f41c69ef13a32afb672d17051524d229fe9a03d
proomguy <mattz100bc@gmail.com>
2023-01-17 12:27:20 +0300

    Forgot password working now

--- lib/forget_password/components/info.dart
+++ lib/forget_password/components/info.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:photo_sharing/account_check/account_check.dart';
 import 'package:photo_sharing/log_in/login_screen.dart';
+import 'package:photo_sharing/sign_up/sign_up_screen.dart';
 import 'package:photo_sharing/widgets/button_square.dart';
 import 'package:photo_sharing/widgets/input_field.dart';
 
 class Credentials extends StatelessWidget{
 
   final TextEditingController _emailTextEditingController = TextEditingController(text: '');
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context){
@@ -36,8 +41,27 @@ class Credentials extends StatelessWidget{
             colors1: Colors.lime,
             colors2: Colors.limeAccent,
             press: () async {
-
+              try{
+                await _auth.sendPasswordResetEmail(email: _emailTextEditingController.text);
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  backgroundColor: Colors.amber,
+                  content: Text("Password Reset email has been sent",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  ),
+                );
+              }
+              on FirebaseAuthException catch(error){
+                Fluttertoast.showToast(msg: error.toString());
+              }
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
             },
+          ),
+          TextButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => SignUpScreen()));
+            },
+            child: const Center(child: Text("Create Account")),
           ),
           const SizedBox(height: 15.0,),
           AccountCheck(
